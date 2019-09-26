d98beeb7011e6c7e42ec8a8a0a5cdcf769188173
giovanni.t.finotello <Gi030201@>
2019-09-26 14:11:15 -0300

    Aula 28 checkbox

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -24,7 +24,8 @@ const routes: Routes = [
   { path: 'component-alert', loadChildren: './component-alert/component-alert.module#ComponentAlertPageModule' },
   { path: 'component-badges', loadChildren: './component-badges/component-badges.module#ComponentBadgesPageModule' },
   { path: 'component-button', loadChildren: './component-button/component-button.module#ComponentButtonPageModule' },
-  { path: 'component-card', loadChildren: './component-card/component-card.module#ComponentCardPageModule' }
+  { path: 'component-card', loadChildren: './component-card/component-card.module#ComponentCardPageModule' },
+  { path: 'component-checkbox', loadChildren: './component-checkbox/component-checkbox.module#ComponentCheckboxPageModule' }
 ];
 
 @NgModule({

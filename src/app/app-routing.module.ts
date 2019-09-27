aee657a7e649c2634008de457a232e729a95cd6d
giovanni.t.finotello <Gi030201@>
2019-09-27 13:32:02 -0300

    Aula 29 Radio button

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -25,7 +25,8 @@ const routes: Routes = [
   { path: 'component-badges', loadChildren: './component-badges/component-badges.module#ComponentBadgesPageModule' },
   { path: 'component-button', loadChildren: './component-button/component-button.module#ComponentButtonPageModule' },
   { path: 'component-card', loadChildren: './component-card/component-card.module#ComponentCardPageModule' },
-  { path: 'component-checkbox', loadChildren: './component-checkbox/component-checkbox.module#ComponentCheckboxPageModule' }
+  { path: 'component-checkbox', loadChildren: './component-checkbox/component-checkbox.module#ComponentCheckboxPageModule' },
+  { path: 'component-radio', loadChildren: './component-radio/component-radio.module#ComponentRadioPageModule' }
 ];
 
 @NgModule({
